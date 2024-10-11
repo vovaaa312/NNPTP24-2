ea2f9ff12ff830653e4e13036af7b4539e49807e
Filip <aaa@aaa.aa>
2024-10-12 00:36:21 +0200

    Added support for all parameters in toJson method

--- src/main/java/cz/upce/fei/nnptp/zz/entity/JSON.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/JSON.java
@@ -6,24 +6,31 @@
 package cz.upce.fei.nnptp.zz.entity;
 
 import java.util.List;
-
+import java.util.HashMap;
 /**
  *
  * @author Roman
  */
 public class JSON {
-    
-    
+
     public String toJson(List<Password> passwords)  {
-        // TODO: support all parameters!!!
         String output = "[";
         for (Password password : passwords) {
             if (!output.isEmpty() && !output.equals("["))
                 output += ",";
             output += "{";
-            output += "id:" + password.getId() + ",";
-            output += "password:\"" + password.getPassword()+"\"";
-            
+            output += "\"id\":" + password.getId() + ",";
+            output += "\"password\":\"" + password.getPassword()+"\"";
+            HashMap<String, Parameter> parameters = password.getParameters();
+            if (parameters != null && !parameters.isEmpty()) {
+                output += ",\"parameters\":{";
+                for (String key : parameters.keySet()) {
+                    Parameter parameter = parameters.get(key);
+                    output += "\"" + key + "\":" + parameter.toJson() + ",";
+                }
+                output = output.substring(0, output.length() - 1);
+                output += "}";
+            }
             output += "}";
         }
         output += "]";
