6f423a43e5611e497cce48e229a11fc241e88523
Bajky11 <lukin.bajer@gmail.com>
2024-11-22 10:29:11 +0100

    Reformatted code according to Java code conventions

--- src/main/java/cz/upce/fei/nnptp/zz/entity/PasswordDatabase.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/PasswordDatabase.java
@@ -47,12 +47,16 @@ public class PasswordDatabase {
     public Password findEntryByTitle(String title) {
         for (Password password : passwords) {
 
-            if (!password.hasParameter(Parameter.StandardizedParameters.TITLE)) continue;
+            if (!password.hasParameter(Parameter.StandardizedParameters.TITLE)){
+                continue;
+            }
 
-            Parameter.TextParameter titleParam;
-            titleParam = (Parameter.TextParameter)password.getParameter(Parameter.StandardizedParameters.TITLE);
+            Parameter.TextParameter titleParam = (Parameter.TextParameter)password.getParameter(Parameter.StandardizedParameters.TITLE);
 
-            if (titleParam.getValue().equals(title)) return password;
+            if (titleParam.getValue().equals(title)){
+                return password;
+            }
+
         }
         return null;
     }
