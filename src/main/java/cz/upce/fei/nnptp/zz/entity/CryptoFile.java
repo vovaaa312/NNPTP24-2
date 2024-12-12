c4c007b92eb8342ab7569a2cc9139fd8a49f34ff
verysvadym <veresivadim3@gmail.com>
2024-12-12 15:09:47 +0100

    thefirstcommit

--- src/main/java/cz/upce/fei/nnptp/zz/entity/CryptoFile.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/CryptoFile.java
@@ -1,22 +1,10 @@
 package cz.upce.fei.nnptp.zz.entity;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.io.*;
+import java.security.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -24,66 +12,34 @@ import javax.crypto.spec.SecretKeySpec;
  * @author Roman
  */
 public class CryptoFile {
-    
-    private static Cipher initializeCipher(String password, int mode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+
+    private static Cipher initializeCipher(String password, int mode) throws GeneralSecurityException {
         Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
         SecretKey secretKey = new SecretKeySpec(password.getBytes(), "DES");
         cipher.init(mode, secretKey);
         return cipher;
     }
-    
+
     public static String readFile(File file, String password) {
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(file);
-            Cipher cipher = initializeCipher(password, Cipher.DECRYPT_MODE);
-            CipherInputStream cis = new CipherInputStream(fis, cipher);
-            
-            DataInputStream dis = new DataInputStream(cis);
-            String fileContent = dis.readUTF();
-            dis.close();
-            cipher.doFinal();
-            
-            return fileContent;
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IOException | InvalidKeyException |
-                 IllegalBlockSizeException | BadPaddingException ex) {
-            Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                if(fis != null){
-                    fis.close();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        try (FileInputStream fis = new FileInputStream(file);
+             CipherInputStream cis = new CipherInputStream(fis, initializeCipher(password, Cipher.DECRYPT_MODE));
+             DataInputStream dis = new DataInputStream(cis)) {
+
+            return dis.readUTF();
+        } catch (Exception ex) {
+            Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, "Failed to read file", ex);
         }
-        
         return null;
     }
-    
-    public static void writeFile(File file, String password, String content) {
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(file);
-            Cipher cipher = initializeCipher(password, Cipher.ENCRYPT_MODE);
-            CipherOutputStream cis = new CipherOutputStream(fos, cipher);
 
-            DataOutputStream dos = new DataOutputStream(cis);
+    public static void writeFile(File file, String password, String content) {
+        try (FileOutputStream fos = new FileOutputStream(file);
+             CipherOutputStream cos = new CipherOutputStream(fos, initializeCipher(password, Cipher.ENCRYPT_MODE));
+             DataOutputStream dos = new DataOutputStream(cos)) {
+
             dos.writeUTF(content);
-            dos.close();
-            cipher.doFinal();
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IOException |
-                 IllegalBlockSizeException | BadPaddingException ex) {
-            Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                if(fos != null) {
-                    fos.close();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (Exception ex) {
+            Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, "Failed to write file", ex);
         }
     }
-    
 }
