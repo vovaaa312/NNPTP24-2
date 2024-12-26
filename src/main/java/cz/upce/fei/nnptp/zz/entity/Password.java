85143934ed928262c4c7c94cd4c7a9aca9524689
verysvadym <veresivadim3@gmail.com>
2024-12-26 12:52:01 +0100

    Thesecondtask

--- src/main/java/cz/upce/fei/nnptp/zz/entity/Password.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/Password.java
@@ -1,7 +1,8 @@
 package cz.upce.fei.nnptp.zz.entity;
 
 import java.util.HashMap;
-
+import java.util.Map;
+import java.util.Objects;
 /**
  * @author Roman
  */
@@ -20,5 +21,20 @@ public record Password(int id, String password, HashMap<String, Parameter> param
         return parameters.get(title);
     }
 
+    public void addParameter(String title, Parameter parameter) {
+        parameters.put(title, parameter);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Password password1 = (Password) o;
+        return id == password1.id && Objects.equals(password, password1.password) && Objects.equals(parameters, password1.parameters);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, password, parameters);
+    }
 }
